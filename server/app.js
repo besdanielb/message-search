04fd7a1b8c1a9837e77fcd18ddc73b43e877669d
Daniel Silva <Daniel.SA.Silva@ctw.bmwgroup.com>
2021-07-13 19:26:49 +0100

    using mock responses from server

--- server/app.js
+++ server/app.js
@@ -2,14 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
 
+const cors = require("cors");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 const port = 3001;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get("/", replyJson("ola"));
-
-app.get("/search-results", replyFile(`responses/searchResults.json`));
+app.get("/search", replyFile(`./responses/searchResults.json`));
+app.get("/message/:date", replyFile(`./responses/message.json`));
+app.get("/", replyJson("This is working"));
 
 function replyFile(file) {
   return function (req, res) {
